fb44037f94c674ba091c04314a4d5c1d697cb554
Lukas Höpfner <lukas.hoepfner@zlyde.com>
2018-07-05 15:20:36 +0200

    refixed

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ var cron = require('node-cron');
 const { exec } = require('child_process');
 
 function runScript() {
-   exec('npm start', (error, stdout) => console.log(stdout));
+   exec('npm run dryRun', (error, stdout) => console.log(stdout));
 }
 
-cron.schedule('*/30 * * * * *', runScript(), null, true, 'Europe/Berlin');
-// cron.schedule('00 01 15 * * *', runScript(), null, true, 'Europe/Berlin');
+cron.schedule('*/30 * * * * *', runScript, null, true, 'Europe/Berlin');
+// cron.schedule('00 01 15 * * *', runScript, null, true, 'Europe/Berlin');
